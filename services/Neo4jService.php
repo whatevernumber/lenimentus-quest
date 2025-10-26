cc9a91a0f51bdbdd9197ad0f476ba994cbc2c619
whatevernumber <the_lost_number@mail.ru>
2025-10-26 16:49:09 +0300

    Add dbUrl

--- services/Neo4jService.php
+++ services/Neo4jService.php
@@ -11,6 +11,7 @@ class Neo4jService
     private string $user;
     private string $password;
     private string $db;
+    private string $dbUrl;
 
     private ClientInterface $client;
 
@@ -19,6 +20,7 @@ class Neo4jService
         $this->user = _env('DB_USER');
         $this->password = _env('DB_PASSWORD');
         $this->db = _env('DB_DATABASE');
+        $this->dbUrl = _env('DB_URL');
         $this->createClient();
     }
 
@@ -29,8 +31,8 @@ class Neo4jService
     {
         $credits = $this->user . ':' . $this->password;
 
-        $this->client = ClientBuilder::create()->withDriver('bolt', 'bolt://' . $credits . '@89.191.225.149:7687') // creates a bolt driver
-        ->withDriver('http', 'http://89.191.225.149', Authenticate::basic('neo4j', $this->password)) // creates an http driver
+        $this->client = ClientBuilder::create()->withDriver('bolt', 'bolt://' . $credits . '@' . $this->dbUrl) // creates a bolt driver
+        ->withDriver('http', 'http://' . $this->dbUrl, Authenticate::basic('neo4j', $this->password)) // creates an http driver
         ->withDriver('neo4j', 'neo4j://neo4j.test.com?database=' . $this->db, Authenticate::oidc('token')) // creates an auto routed driver with an OpenID Connect token
         ->withDefaultDriver('bolt')
         ->build();
