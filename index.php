bc3f3b554279cfbc76e4d7fb92aa318e1e75b907
whatevernumber <the_lost_number@mail.ru>
2025-12-27 22:08:59 +0300

    refactoring

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\Neo4jService;
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -13,33 +14,11 @@ app()->cors([
 
 app()->post('/api/quest', 'App\Controllers\StageController@processAction');
 
-    $visitedStages = app()->request()->get('stages') ?? null;
-    $currentAction = app()->request()->get('action') ?? null;
-    $currentStage = app()->request()->get('stage') ?? null;
 
     $service = new Neo4jService();
 
-    if ($currentAction) {
-        $currentAction = mb_ucfirst($currentAction);
-        $relation = Stage::selectOptionRelation($currentAction, $visitedStages, $currentStage);
 
-        $stage = $service->getStageByAction($currentAction, $relation);
-        $options = $service->getStageOptions($stage[0]['quest.stage']);
-    } else {
-        $stage = $service->getStage($currentStage);
-        $options = $service->getStageOptions($currentStage);
-    }
 
-    if ($visitedStages) {
-       $options = Stage::filterSpecialConditionActions($options, $visitedStages, $currentStage);
-    }
-
-    $result = [
-        'stage' => $stage,
-        'actions' => $options,
-    ];
-
-    response()->json($result);
 });
 
 app()->run();
